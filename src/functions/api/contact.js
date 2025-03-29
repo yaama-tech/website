df71710810fd8d5bfa7c4e956cf6806b4b98e8f9
Aboo <a.azarnoush@yaama.tech>
2025-03-30 10:30:18 +1100

    test

--- src/functions/api/contact.js
+++ src/functions/api/contact.js
@@ -1,3 +1,8 @@
-export function onRequest(context) {
+// This function is used to send a message to the contact form
+// It is triggered when a user submits the contact form
+// This is a Cloudflare Worker function
+export async function onRequest(context) {
+    const { request } = context;
+    const { name, email, message } = await request.json();
     return new Response("Thank you for your message! We will get back to you as soon as possible.")
 }
